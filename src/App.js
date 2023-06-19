c57e33af352ff20b971afc4e5405c39054ac5cc9
parikhasharma <sharmaparikha@gmail.com>
2023-06-19 11:51:49 +0530

    react-redux-demo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import CakeContainer from './components/CakeContainer';
+import {Provider} from 'react-redux'
+import store from './redux/store';
+import './App.css';
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+      <CakeContainer/>
+    </div>
+    </Provider>
+    
+   
+  );
+}
+
+export default App;
